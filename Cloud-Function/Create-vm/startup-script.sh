8a471f54fcfcd828feff00720acf50249278e9e0
aldanilo <30423596+aldanilo@users.noreply.github.com>
2019-09-23 10:02:04 -0300

    Update startup-script.sh

--- Cloud-Function/Create-vm/startup-script.sh
+++ Cloud-Function/Create-vm/startup-script.sh
@@ -4,12 +4,13 @@ apt-get install update
 #Acessando os metadados da VM setados no deploy
 #Aqui pode colocar os nomes das imagens, bucket de acesso e bucket de destino. 
 #Qualquer variável pode ser passada por aqui.
-BUCKET=$(curl http://metadata/computeMetadata/v1/instance/attributes/bucket -H "Metadata-Flavor: Google")
-ZONE=$(curl http://metadata/computeMetadata/v1/instance/attributes/zone -H "Metadata-Flavor: Google")
-INSTANCE_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/instance_name -H "Metadata-Flavor: Google")
-FILE_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/file_name -H "Metadata-Flavor: Google")
-PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+export BUCKET=$(curl http://metadata/computeMetadata/v1/instance/attributes/bucket -H "Metadata-Flavor: Google")
+export ZONE=$(curl http://metadata/computeMetadata/v1/instance/attributes/zone -H "Metadata-Flavor: Google")
+export INSTANCE_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/instance_name -H "Metadata-Flavor: Google")
+export FILE_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/file_name -H "Metadata-Flavor: Google")
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 
+echo $BUCKET $ZONE $INSTANCE_NAME $FILE_NAME $PROJECT_ID> randomtext.txt
 
 #Comandos de copiar a aplicação do GS para a VM
 #gsutil cp gs://$BUCKET/app.py /app/
