9a6a6001f559c6511e4ed9e36dedb6d0d200d28d
aldanilo <30423596+aldanilo@users.noreply.github.com>
2019-09-25 15:40:20 -0300

    Create startup-script.sh

---
+++ Cloud-Function/Create_vm_disk/startup-script.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -x
+
+apt-get update
+
+BUCKET=$(curl http://metadata/computeMetadata/v1/instance/attributes/bucket -H "Metadata-Flavor: Google")
+ZONE=$(curl http://metadata/computeMetadata/v1/instance/attributes/zone -H "Metadata-Flavor: Google")
+INSTANCE_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/instance_name -H "Metadata-Flavor: Google")
+FILE_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/file_name -H "Metadata-Flavor: Google")
+DELETE_INSTANCE=$(curl http://metadata/computeMetadata/v1/instance/attributes/delete -H "Metadata-Flavor: Google")
+PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
+cat > ~/delete_instance.sh <<EOF
+PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+ZONE=$(curl http://metadata/computeMetadata/v1/instance/attributes/zone -H "Metadata-Flavor: Google")
+INSTANCE_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/instance_name -H "Metadata-Flavor: Google")
+
+curl -X POST "https://us-central1-refreshing-mark-252714.cloudfunctions.net/delete-vm" -H "Content-Type:application/json" \
+--data '{"project": "'$PROJECT_ID'","zone": "'$ZONE'","name": "'$INSTANCE_NAME'"}'
+EOF
+
+chmod +x ~/delete_instance.sh
+
+sleep 60
+
+bash ~/delete_instance.sh
